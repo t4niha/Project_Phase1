f241f4cdab206a0cfe5e2a201523d2a5dfd998fb
Akif Zahin <zahinakif25@gmail.com>
2024-11-16 02:21:31 +0600

    Update prediction.py

--- backend/prediction.py
+++ backend/prediction.py
@@ -20,7 +20,7 @@ model.eval()
 
 """# **Prediction Test**"""
 # Example text for prediction
-text = ""
+text = "President-elect Trump announced two more additions to his White House staff on Friday, promoting his campaign spokesman to lead the communications shop.Trump announced that Steven Cheung would return to the White House as assistant to the president and director of communications. Cheung previously served as communications director for the Trump-Vance campaign and was the White House director of strategic response in Trump's first term.Additionally, Trump confirmed that Sergio Gor will join the White House as director of the presidential personnel office. Gor, an ally and business partner of Donald Trump Jr.'s, was in charge of the pro-Trump political action committee Right For America and previously worked in Republican Sen. Rand Paul's office. Steven Cheung and Sergio Gor have been trusted Advisors since my first Presidential Campaign in 2016, and have continued to champion America First principles throughout my First Term, all the way to our HIstoric Victory in 2024, Trump said in a statement. I am thrilled to have them join my White House, as we Make America Great Again!" 
 # Tokenize input
 inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512)
 
